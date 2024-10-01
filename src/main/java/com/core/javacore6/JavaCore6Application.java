9c9f064f0e7c98217f4165352337b1ef1b73606b
danil <daniltihomirov946@gmail.com>
2024-10-01 14:44:13 +0300

    Initial commit

---
+++ src/main/java/com/core/javacore6/JavaCore6Application.java
@@ -0,0 +1,13 @@
+package com.core.javacore6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCore6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCore6Application.class, args);
+	}
+
+}
