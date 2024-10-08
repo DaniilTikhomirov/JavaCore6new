b52fee494a064fe9bbdcbd6f94438ee348ce4914
danil <daniltihomirov946@gmail.com>
2024-10-08 12:53:28 +0300

    change method to stream

--- src/main/java/com/core/javacore6/services/EmployeeDepartmentServiceImpl.java
+++ src/main/java/com/core/javacore6/services/EmployeeDepartmentServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class EmployeeDepartmentServiceImpl implements EmployeeDepartmentService {
@@ -16,31 +17,35 @@ public class EmployeeDepartmentServiceImpl implements EmployeeDepartmentService 
 
     @Override
     public List<Employee> getEmployeesForDepartment(int department) {
-        List<Employee> employees = employeeService.getEmployeeList();
-        return employees.stream().
-                filter(employee -> employee != null && employee.getDepartment() == department).toList();
+        return getStreamEmployeesForDepartment(department).toList();
     }
 
-    private double minSalary(List<Employee> employees) {
-        return employees.stream().min(Comparator.comparingDouble(Employee::getSalary)).
+    private Stream<Employee> getStreamEmployeesForDepartment(int department) {
+        List<Employee> employees = employeeService.getEmployeeList();
+        return employees.stream().
+                filter(employee -> employee != null && employee.getDepartment() == department);
+    }
+
+    private double minSalary(Stream<Employee> employees) {
+        return employees.min(Comparator.comparingDouble(Employee::getSalary)).
                 orElseThrow(() -> new EmployeeNotFoundException("не найден сотрудник")).getSalary();
     }
 
-    private double maxSalary(List<Employee> employees) {
-        return employees.stream().max(Comparator.comparingDouble(Employee::getSalary)).
+    private double maxSalary(Stream<Employee> employees) {
+        return employees.max(Comparator.comparingDouble(Employee::getSalary)).
                 orElseThrow(() -> new EmployeeNotFoundException("не найден сотрудник")).getSalary();
     }
 
 
     @Override
     public double minSalaryForDepartment(int department) {
-        List<Employee> employee = getEmployeesForDepartment(department);
+        Stream<Employee> employee = getStreamEmployeesForDepartment(department);
         return minSalary(employee);
     }
 
     @Override
     public double maxSalaryForDepartment(int department) {
-        List<Employee> employee = getEmployeesForDepartment(department);
+        Stream<Employee> employee = getStreamEmployeesForDepartment(department);
         return maxSalary(employee);
     }
 
